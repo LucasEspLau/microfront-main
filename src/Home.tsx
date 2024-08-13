30d821d8b52ba67fc5ba9edca644e785da59eaf1
LucasEspLau <lukaespinoza12@gmail.com>
2024-08-13 10:21:40 -0500

    cambio de front main

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 export default function Home() {
   const [count, setCount] = useState(0)
+  const iframeRef = useRef<HTMLIFrameElement | null>(null);
 
+  // Enviar datos al iframe cuando count cambie
+  useEffect(() => {
+    if (iframeRef.current) {
+      iframeRef.current.contentWindow?.postMessage({ count }, '*');
+    }
+  }, [count]);
   return (
     <>
       <div>
@@ -29,6 +36,7 @@ export default function Home() {
         Click on the Vite and React logos to learn more
       </p>
       <iframe 
+        ref={iframeRef} 
         src="https://main.d2pafslliiho2h.amplifyapp.com" 
         style={{ width: '100%', height: '100vh', border: 'none' }} 
         title="Microfront-2"
